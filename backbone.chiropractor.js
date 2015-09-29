7eb1a490a0ce7f741d07299e7cbc3a079d21a2ff
Chad Dotson <chad@cdotson.com>
2015-09-28 22:04:12 -0500

    Cleanup console.logs and commented code.

--- backbone.chiropractor.js
+++ backbone.chiropractor.js
@@ -33,57 +33,33 @@
     var BackboneOrphanDetector =  (function() {
         var instance;
 
-
         var _records = [];
-
-        var _trackers = {};
         var _source;
-
-        var _self;
-
-
-
 
         function _start() {
 
             var _self = this;
 
-            function _doSelfTrigger() {
-                console.log("TRIGGER on Backbone.Chiropractor ---------------------------------------------");
-                _self.trigger("trigger", arguments);
-            };
-
-
             _source = _.extend({}, Backbone.Events);
 
             Events = Backbone.Events = {
                 on: function () {
-                    console.log("in on");
                     _source.on.apply(this, arguments);
                 },
                 off: function () {
-                    console.log("in off");
                     _source.off.apply(this, arguments);
                 },
                 trigger: function() {
-                    console.log("in trigger",_self);
                     _source.trigger.apply(this, arguments);
-                    _doSelfTrigger();
+                    _self.trigger("trigger", arguments);
                 },
                 listenTo: function (obj, name, callback) {
                     var self = this;
                     var stack = Error().stack;
                     _source.listenTo.apply(this, arguments);
-                    console.log("in listenTo", arguments);
-                    console.log("this", this._listenId);
-                    console.log("name", name);
-
-                    //console.log(this._listenId, name, Error().stack);
 
                     if(_.isObject(name)) {
-                        //console.log("keys", _.keys(name));
                         _.each(_.keys(name), function (event) {
-                            //console.log("tracker", self);
                             _addTracker(_self, obj, event, stack);
                         });
                     } else {
@@ -95,8 +71,6 @@
                             _addTracker(self, obj, names[i], stack)
                         }
                     }
-
-                    console.log("-------------- Done wiring up listener --------------")
                 },
                 once: function () {
                     console.log("in once");
@@ -132,24 +106,12 @@
         }
 
         function _dumpListeners() {
-            console.log("Dumping listeners");
+            console.log("Listeners");
             console.log(_records);
-            //console.log(Object.keys(_trackers).length);
-
-            //for(var listenerId in _trackers) {
-            //    for(var listeneeId in _trackers[listenerId]) {
-            //        for(var event in _trackers[listenerId][listeneeId]) {
-            //            console.log(listenerId, listeneeId, event, _trackers[listenerId][listeneeId][event].stack);
-            //        }
-            //    }
-            //}
         }
 
         function _addTracker(listener, listenee, event, stackOnCreation) {
             console.log("listener", listener, "listenee", listenee);
-
-            var listenerId = listener._listenId;
-            var listeneeId = listenee._listenId;
 
             _records.push({
                 listener: listener,
@@ -157,28 +119,11 @@
                 event: event,
                 stack: stackOnCreation
             });
-
-
-
-
-            _trackers[listenerId] = _trackers[listenerId] || {};
-            _trackers[listenerId][listeneeId] = _trackers[listenerId][listeneeId] || {};
-
-            _trackers[listenerId][listeneeId][event] = {
-                listener: listener,
-                listenee: listenee,
-                event: event,
-                stack: stackOnCreation
-            };
-
-            console.log("trackers", _trackers);
-
-
         }
 
         function _removeTracker(id) {
 
-            delete _trackers[id];
+            //delete _trackers[id];
         }
 
         function init() {
@@ -202,201 +147,8 @@
 
     })();
 
-    console.log("Before:", BackboneOrphanDetector.getInstance());
-
     _.extend(BackboneOrphanDetector.getInstance(), Backbone.Events);
-    console.log("After:", BackboneOrphanDetector.getInstance());
 
     return BackboneOrphanDetector.getInstance();
 
 }));
-
-
-/*
-
-
-
-
-
-
-
-
-
-// it should be able to cleanly replace backbone.events
-// it should be able to cleanly reenable the regular backbone.events
-// it should be able to listen to all events on an object
-// it should be able to listen to specific events on an object
-// it should be able to stop listening to all events on an object.
-// it should be able to stop listening to specific events.
-
-
-
-var BackboneOrphanDetector = (function() {
-    var instance;
-    
-    var _trackers = {};
-  	var _source;
-
-    function _start() {
-        _source = _.extend({}, Backbone.Events);
-        Events = Backbone.Events = {
-            on: function () {
-    			console.log("in on");
-        		_source.on.apply(this, arguments);
-    		},
-            off: function () {
-                console.log("in off");
-		        _source.off.apply(this, arguments);
-            },
-            trigger: function() {
-                console.log("in trigger");
-                _source.trigger.apply(this, arguments);
-            },
-            listenTo: function (obj, name, callback) {
-                _source.listenTo.apply(this, arguments);
-                console.log("in listenTo", arguments);
-                console.log("this", this);
-                console.log("name", name);
-                var self = this;
-                //console.log(this._listenId, name, Error().stack);
-
-                if(_.isObject(name)) {
-                    console.log("keys", _.keys(name));
-                    _.each(_.keys(name), function (event) {
-                        console.log("tracker", self);
-                        _addTracker(self, obj, event, Error().stack);
-                    });
-                } else {
-                    // todo: gotta split name.
-                    console.log("Not implemented");
-                    //_addTracker(this, obj, name, Error().stack);
-                }
-            },
-            once: function () {
-                console.log("in once");
-                source.once.apply(this, arguments);
-            },
-
-            listenToOnce: function () {
-                console.log("in listenToOnce");
-                _source.listenToOnce.apply(this, arguments);
-            },
-            
-            stopListening: function () {
-                console.log("in stopListening", this._listenId);
-                _removeTracker(this._listenId);
-                _source.stopListening(this, arguments);
-            }
-        };
-    };
-    
-    function _stop() {
-        Events = Backbone.Events = _source;
-    };    
-    
-    function _dumpListeners() {
-        console.log("Dumping listeners");
-//        console.log(_trackers);
-        //console.log(Object.keys(_trackers).length);
-
-        for(var listenerId in _trackers) {
-            for(var listeneeId in _trackers[listenerId]) {
-                for(var event in _trackers[listenerId][listeneeId]) {
-                    console.log(listenerId, listeneeId, event, _trackers[listenerId][listeneeId][event].stack);
-                }
-            }
-        }
-    }
-        
-    function _addTracker(listener, listenee, event, stackOnCreation) {
-        console.log("listener", listener._listenId, "listenee", listenee._listenId);
-
-        var listenerId = listener._listenId;
-        var listeneeId = listenee._listenId;
-        
-        _trackers[listenerId] = _trackers[listenerId] || {};
-        _trackers[listenerId][listeneeId] = _trackers[listenerId][listeneeId] || {};
-        
-        _trackers[listenerId][listeneeId][event] = {
-            listener: listener,
-            listenee: listenee,
-            event: event,
-            stack: stackOnCreation
-        };
-
-        console.log("trackers", _trackers);
-
-
-    }
-        
-    function _removeTracker(id) {
-    	
-        delete _trackers[id];
-    }
-
-    function init() {
-        return {
-            start: _start,
-			stop: _stop,
-            dumpListeners: _dumpListeners,
-        };   
-    };
-    
-    return {
-        getInstance: function () {
-            if(!instance) {
-                instance = init();
-            }
-            return instance;
-        }
-    }
-    
-})();
-
-
-
-var orphanDetector = BackboneOrphanDetector.getInstance();
-
-orphanDetector.start();
-
-var MyClass = function () {
-	this.say = function(what) {
-        console.log(what);
-        this.trigger("MyClass:said", what);
-    };
-}
-
-var MyClass2 = function () {
-	this.say = function(what) {
-        console.log(what);
-        this.trigger("MyClass2:said", what);
-    };
-}
-
-_.extend(MyClass.prototype, Backbone.Events);
-_.extend(MyClass2.prototype, Backbone.Events);
-
-
-
-var x = new MyClass();
-
-function tester() {
-    var y = new MyClass2();
-
-    y.listenToOnce(x, "MyClass:said", function(what) {
-        //console.log("you said " + what);
-        y.say("you said: " + what);
-        orphanDetector.stop();
-    });
-}
-
-tester();
-
-orphanDetector.dumpListeners();
-//x.say("test");
-setTimeout(function () {
-    x.say("test");
-    orphanDetector.dumpListeners();
-}, 5000);
-
-    */
